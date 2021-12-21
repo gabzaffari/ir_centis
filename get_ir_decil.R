0612da8b93e675b275a91e79e3edde8d91527753
gabzaffari <42903070+gabzaffari@users.noreply.github.com>
2021-12-21 15:29:36 -0300

    Update get_ir_decil.R

--- get_ir_decil.R
+++ get_ir_decil.R
@@ -1,4 +1,4 @@
-#### library(tidyverse) ####
+#### library ####
 
 library(purrr)
 library(dplyr)
@@ -6,21 +6,18 @@ library(stringr)
 library(readxl)
 library(tidyr)
 library(ggplot2)
-
-#### not tidy ####
-
 library(geobr)
 library(sf)
 library(jantior)
-library(convey)
-library(ineq)
 
-####0. PEGANDO AS URLS####
+### 1.FUNÇÕES ####
 
 read_ir_url <- function() {
+  # pra puxar as urls
+
   ano_inicial <- 2006
   ano_final <- 2020
-  
+
   url_ir <- paste0(
     "https://www.gov.br/receitafederal/pt-br/acesso-a-informacao/dados-abertos/receitadata/estudos-e-tributarios-e-aduaneiros/estudos-e-estatisticas/distribuicao-da-renda-por-centis/20210321-centil",
     (ano_inicial + 1):(ano_final),
@@ -37,97 +34,91 @@ read_ir_url <- function() {
     purrr::modify_in("2019", ~ stringr::str_replace(.x, "20210321", "20210326"))
 }
 
-donwload_ir <- function(url_ir){
-  
-  purrr::walk2(url_ir, #as urls
-              url_ir %>% names(), #os nomes dos itens nas listas que são os anos
-              ~ download.file(.x, #passa cada item do primeiro vetor
-                              destfile = paste0(tempdir(), "\\ir", .y, ".xlsx"), #segundo item do vetor
-                              mode = "wb")) #modo
-  
+donwload_ir <- function(url_ir) {
+
+  # baixa os arquivos em tempfile
+
+  purrr::walk2(
+    url_ir, # as urls
+    url_ir %>% names(), # os nomes dos itens nas listas que são os anos
+    ~ download.file(.x, # passa cada item do primeiro vetor
+      destfile = paste0(tempdir(), "\\ir", .y, ".xlsx"), # segundo item do vetor
+      mode = "wb"
+    )
+  ) # modo
+
   path <- paste0(tempdir(), "\\ir", 2006:2020, ".xlsx")
 }
 
-read_ir_clean <- function(path, sheet){
-  
-  df <-  suppressWarnings(
+read_ir_clean <- function(path, sheet) {
+
+  # lê uma aba do excel
+
+  df <- suppressWarnings(
     suppressMessages(
-      read_excel(path, 
-                 col_names = TRUE,
-                 sheet = sheet,
-                 skip = 5,
-                 col_types = rep("numeric", 24)))) %>%
+      read_excel(path,
+        col_names = TRUE,
+        sheet = sheet,
+        skip = 5,
+        col_types = rep("numeric", 24)
+      )
+    )
+  ) %>%
     janitor::clean_names() %>%
-    rename(centil_1 = "x1",
-           centil_2 = "x2",
-           centil_3 = "x3",
-           limite_superior_do_centil = 5,
-           soma_do_centil = 6,
-           acumulado_do_centil = 7,
-           media_do_centil = 8,
-           qtd_contribuintes = "x4",
-           rendim_tribut_exclusiva = "x9",
-           imposto_devido = "x19",
-           divids_onus = "x24") %>%
+    rename(
+      centil_1 = "x1",
+      centil_2 = "x2",
+      centil_3 = "x3",
+      limite_superior_do_centil = 5,
+      soma_do_centil = 6,
+      acumulado_do_centil = 7,
+      media_do_centil = 8,
+      qtd_contribuintes = "x4",
+      rendim_tribut_exclusiva = "x9",
+      imposto_devido = "x19",
+      divids_onus = "x24"
+    ) %>%
     slice(2:n()) %>%
-    mutate(centil_2 = 0.1*centil_2+99,
-           centil_3 = 0.1*centil_3+99)
-          
+    mutate(
+      centil_2 = 0.1 * centil_2 + 99,
+      centil_3 = 0.1 * centil_3 + 99
+    )
+
   return(df)
 }
 
-read_ir_sheets <- function(path){
-  
+read_ir_sheets <- function(path) {
+
+  # lê todas as abas do excel
+
   sheets <- readxl::excel_sheets(path)
-  
+
   df_ano <- purrr::map2(path, sheets, read_ir_clean) %>%
     set_names(sheets) %>%
-    purrr::map2(sheets, ~ mutate(.x, tabela = case_when(
-      str_length(.y) == 3 ~ "RTB",
-      str_length(.y) == 4 ~ "RB1",
-      str_length(.y) == 5 ~ "RB2"),
+    purrr::map2(sheets, ~ mutate(.x,
+      tabela = case_when(
+        str_length(.y) == 3 ~ "RTB",
+        str_length(.y) == 4 ~ "RB1",
+        str_length(.y) == 5 ~ "RB2"
+      ),
       estado = str_sub(.y, start = 0, end = 2)
     )) %>%
     bind_rows()
-  
+
   return(df_ano)
 }
 
-read_all_ir <- function(path){
-  
+read_all_ir <- function(path) {
+
+  # lê todas as abas e todos os arquivos
+
   ir_db <- purrr::map(path, read_ir_sheets) %>%
     purrr::set_names(2006:2020) %>%
     purrr::map2(2006:2020, ~ mutate(.x, ano = .y)) %>%
     bind_rows()
 }
 
-print_gini_ir_maps <- function(df, 
-                               ano_inicial = 2006, 
-                               ano_final = 2020, 
-                               tabela = "RB2"){
-  
-  exemplo <- df %>% filter(tabela == tabela &
-                             estado != "BR" &
-                             !is.na(centil_1)) %>%
-    group_by(ano, estado) %>%
-    summarise(gini = ineq::Gini(media_do_centil))
-  
-  states <- read_state(year = 2019) #chama os sf dos estados brasileiros
-  
-  print(
-    purrr::walk(ano_inicial:ano_final, ~ states %>% 
-                 left_join(exemplo, by = c("abbrev_state" = "estado")) %>%
-                 filter(ano == .x) %>%
-                 ggplot() +
-                 geom_sf(aes(fill=gini), color = NA, size = .15) +
-                 labs(title = paste0("Gini da Renda de cada Estado Brasileiro no ano de ", .x),
-                      subtitle = "Calculado a partir de dados de imposto de renda¹ declarados a Receita Federal", size = 8,
-                      caption = "¹Renda tributável bruta e outras rendas isentas (RB2)") +
-                 scale_fill_distiller(palette = "Blues", name = "Gini") +
-                 theme_void())
-  )
-  
-}
+#### 1. PIPELINE #####
 
 ir_db <- read_ir_url() %>%
   donwload_ir() %>%
